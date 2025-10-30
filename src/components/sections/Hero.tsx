5e23b1e8f5bf375f6a990b8f68ddebd0f1f0e90e
SocialiQ Digital <socialiqdigital@gmail.com>
2025-10-30 15:13:00 +0000

    update the link under "View My Resume" button with "https://drive.google

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
             Motivated Professional With Expertise in Administration, Digital Marketing, and AI-Powered Solutions.
             </p>
             <Button asChild size="lg">
-              <Link href="https://drive.google.com/uc?export=download&id=1V2zk6Ecx8TSFqZGxFNAFxXFLnxUe7Ccb" target="_blank" rel="noopener noreferrer">
+              <Link href="https://drive.google.com/file/d/1nQOBtHfB43e9t4pedjrO7f691mpsh7s3/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
                 View My Resume
               </Link>
             </Button>
